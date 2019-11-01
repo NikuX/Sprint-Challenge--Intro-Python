72c8edd3c1dabbb38771ed1dbb6edeb88c3acb9d
NikuX <44727534+NikuX@users.noreply.github.com>
2019-11-01 22:00:42 +0000

    small changes

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,8 +4,8 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self):
-        self.num_wheels = 4
+    def __init__(self, num_wheels=4):
+        self.num_wheels = num_wheels
 
     def drive(self):
         print('vroooom')
@@ -19,8 +19,8 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 class Motorcycle(GroundVehicle):
-    def __init__(self):
-        self.num_wheels = 2
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
 
     def drive(self):
         print('BRAAAP!!')
